c2d3322f681a3abcc1cbd7286fd4b4cfa88a72fb
syahidanAS <syahidan139@gmail.com>
2020-11-02 17:02:12 +0700

    Ganti database

---
+++ application/controllers/Login_user.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Login_user extends CI_Controller {
+ 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('User_m');
+	}
+ 
+	public function index()
+	{
+		$this->load->view('login_form');
+	}
+ 
+	public function proses()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		if($this->User_m->login_user($username,$password))
+		{
+			redirect('Admin');
+		}
+		else
+		{
+			$this->session->set_flashdata('error','Username & Password salah');
+			redirect('login');
+		}
+	}
+ 
+	public function logout()
+	{
+		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('nama');
+		$this->session->unset_userdata('is_login');
+		redirect('login');
+	}
+ 
+	
+ 
+}
